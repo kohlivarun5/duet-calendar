cc7166706a72f0a2b84c57b31a35baec6c3240af
Varun Kohli <kohli.varun5@gmail.com>
2026-07-10 00:29:47 -0400

    Route organic pages to approved product pages

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,10 +2,27 @@
   var appStoreLinks = document.querySelectorAll(".js-app-store-link");
   var pageSlug = document.body.dataset.pageSlug || window.location.pathname.split("/").filter(Boolean).pop() || "home";
   var campaignToken = "duet_web_" + pageSlug.replace(/[^a-z0-9]+/gi, "_").toLowerCase() + "_202607";
+  var holidayPageSlugs = ["holiday-custody-schedule", "summer-custody-schedule", "school-break-custody-schedule"];
+  var schedulePageSlugs = [
+    "custody-schedule-calculator",
+    "2-2-3-custody-schedule",
+    "2-2-5-5-custody-schedule",
+    "3-4-4-3-custody-schedule",
+    "week-on-week-off-custody-schedule",
+    "50-50-custody-calendar",
+    "alternating-weekends-custody-schedule",
+    "custody-handoff-calendar",
+  ];
+  var productPageID = holidayPageSlugs.includes(pageSlug)
+    ? "f5d8dd2d-228a-40c6-b74d-49815c1ca634"
+    : schedulePageSlugs.includes(pageSlug)
+      ? "87fdba48-108d-4ed2-8710-4772189f6bb2"
+      : "a4de00f6-f673-4e65-9ccd-a66aff48824a";
 
   appStoreLinks.forEach(function (link) {
     var destination = new URL(link.href);
     destination.searchParams.set("ct", campaignToken);
+    destination.searchParams.set("ppid", productPageID);
     link.href = destination.toString();
   });
 
@@ -43,6 +60,7 @@
       var clickEventParams = {
         cta_location: link.dataset.ctaLocation || "unknown",
         campaign_token: campaignToken,
+        custom_product_page_id: productPageID,
         link_url: destination,
         transport_type: "beacon",
       };
